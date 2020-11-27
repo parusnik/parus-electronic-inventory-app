c434f7db7a52cc96de7a1eaf7ee48dbcc17edd0f
Alexander Bocharov <alex.bocharov@outlook.com>
2020-11-27 12:20:29 +0300

    Export DocumentId

--- src/p_beldicplace_csvexp.sql
+++ src/p_beldicplace_csvexp.sql
@@ -1,17 +1,39 @@
 create or replace procedure p_beldicplace_csvexp
 (
   ncompany      in number,
-  nident        in number
+  nident        in number,
+  sdoc_numb     in varchar2,
+  sdoc_pref     in varchar2,
+  sdoc_type     in varchar2
 )
 as
   l_headers     pkg_std.tstring;
   l_row         pkg_std.tlstring;
   l_content     clob;
   l_filename    pkg_std.tstring;
+  l_doctype     pkg_std.tref;
+  l_elinventory pkg_std.tref;
 begin
+  /* определение типа документа */
+  find_doctypes_code_ex(0, 0, ncompany, sdoc_type, l_doctype);
+
+  /* определение документа инвентаризации */
+  begin
+    select rn
+      into l_elinventory
+      from elinventory
+     where company = ncompany
+       and doc_numb = sdoc_numb
+       and doc_pref = sdoc_pref
+       and doc_type = l_doctype;
+  exception
+    when no_data_found then
+      p_exception(0, 'Не удалось определить идентификатор для документа электронной инвентаризации "%s"', pkg_document.make_number(sdoc_type, sdoc_pref, sdoc_numb));
+  end;
+
   dbms_lob.createtemporary(l_content, true);
 
-  l_headers := 'LocationName;LocationSku;' || cr;
+  l_headers := 'DocumentId;LocationName;LocationSku;' || cr;
   dbms_lob.writeappend(l_content, length(l_headers), l_headers);
 
   for rec in 
@@ -25,7 +47,7 @@ begin
        and t.rn = sl.document
   ) 
   loop
-    l_row := rec.LocationName || ';' || rec.LocationSku || ';' || cr;
+    l_row := l_elinventory || ';' || rec.LocationName || ';' || rec.LocationSku || ';' || cr;
     dbms_lob.writeappend(l_content, length(l_row), l_row);
   end loop;
 
